jayaraj/dashboard

commit 3c48aa47896b358f08462afe2be1933229352ac9

Migration: Edit notification channel (#25980)
* implement edit page

* connectWithCleanup

* remove angular related code

* use loadingindicator

* use the correct loading component

* handle secureFields

* fixed implementation of secure fields

* Keep secureFields after rerendering the form

* CollapsableSection and Page refactor

* use checkbox instead of switch

* fix comment

* add cursor to section

* Fixed issues after PR review

* Fix issue with some settings being undefined

* new reducer and start with test

* algorithm to migrate secure fields

* UX: Minor UI Tweaks

* Added field around checkboxes, and missing required field

* fixed test

* tests for util

* minor tweaks and changes

* define as records

* fix typ error

* forward invalid to textarea and inputcontrol

* merge formdata and redux data in test

* fix issue with creating channel

* do not figure out securefields in migration

Co-authored-by: Torkel Ödegaard <torkel@grafana.com>

pkg/services/alerting/notifiers/pagerduty.go
@@ -20,53 +20,6 @@ func init() {
 		Description: "Sends notifications to PagerDuty",
 		Heading:     "PagerDuty settings",
 		Factory:     NewPagerdutyNotifier,
-		OptionsTemplate: `
-      <h3 class="page-heading">PagerDuty settings</h3>
-      <div class="gf-form">
-        <span class="gf-form-label width-14">Integration Key</span>
-		<div class="gf-form gf-form--grow" ng-if="!ctrl.model.secureFields.integrationKey">
-			<input type="text"
-				class="gf-form-input max-width-22"
-				ng-init="ctrl.model.secureSettings.integrationKey = ctrl.model.settings.integrationKey || null; ctrl.model.settings.integrationKey = null;"
-				ng-model="ctrl.model.secureSettings.integrationKey"
-				placeholder="Pagerduty Integration Key"
-				data-placement="right">
-			</input>
-		</div>
-		<div class="gf-form" ng-if="ctrl.model.secureFields.integrationKey">
-			<input type="text" class="gf-form-input max-width-18" disabled="disabled" value="configured" />
-			<a class="btn btn-secondary gf-form-btn" href="#" ng-click="ctrl.model.secureFields.integrationKey = false">reset</a>
-		</div>
-      </div>
-      <div class="gf-form">
-        <span class="gf-form-label width-14">Severity</span>
-        <div class="gf-form-select-wrapper width-14">
-          <select
-            class="gf-form-input"
-            ng-model="ctrl.model.settings.severity"
-            ng-options="s for s in ['critical', 'error', 'warning', 'info']">
-          </select>
-        </div>
-      </div>
-      <div class="gf-form">
-        <gf-form-switch
-           class="gf-form"
-           label="Auto resolve incidents"
-           label-class="width-14"
-           checked="ctrl.model.settings.autoResolve"
-           tooltip="Resolve incidents in pagerduty once the alert goes back to ok.">
-        </gf-form-switch>
-      </div>
-      <div class="gf-form">
-        <gf-form-switch
-           class="gf-form"
-           label="Include message in details"
-           label-class="width-14"
-           checked="ctrl.model.settings.messageInDetails"
-           tooltip="Move the alert message from the PD summary into the custom details. This changes the custom details object and may break event rules you have configured">
-        </gf-form-switch>
-      </div>
-    `,
 		Options: []alerting.NotifierOption{
 			{
 				Label:        "Integration Key",
@@ -101,9 +54,15 @@ func init() {
 			},
 			{
 				Label:        "Auto resolve incidents",
-				Element:      alerting.ElementTypeSwitch,
+				Element:      alerting.ElementTypeCheckbox,
 				Description:  "Resolve incidents in pagerduty once the alert goes back to ok.",
 				PropertyName: "autoResolve",
+			},
+			{
+				Label:        "Include message in details",
+				Element:      alerting.ElementTypeCheckbox,
+				Description:  "Move the alert message from the PD summary into the custom details. This changes the custom details object and may break event rules you have configured",
+				PropertyName: "messageInDetails",
 			},
 		},
 	})

packages/grafana-ui/src/components/Collapse/CollapsableSection.mdx
@@ -1,0 +1,10 @@
+import { Meta, Props } from '@storybook/addon-docs/blocks';
+import { CollapsableSection } from './CollapsableSection';
+
+<Meta title="MDX|CollapsableSection" component={CollapsableSection} />
+
+# Collapsable Section
+A simple container for enabling collapsing/expanding of content.
+
+
+<Props of={CollapsableSection} />

packages/grafana-ui/src/components/Collapse/CollapsableSection.story.tsx
@@ -1,0 +1,21 @@
+import React from 'react';
+import { CollapsableSection } from './CollapsableSection';
+import mdx from './CollapsableSection.mdx';
+
+export default {
+  title: 'Layout/CollapsableSection',
+  component: CollapsableSection,
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+};
+
+export const simple = () => {
+  return (
+    <CollapsableSection label="Collapsable section" isOpen>
+      <div>Here's some content</div>
+    </CollapsableSection>
+  );
+};

packages/grafana-ui/src/components/Collapse/CollapsableSection.tsx
@@ -1,0 +1,38 @@
+import React, { FC, ReactNode, useState } from 'react';
+import { css } from 'emotion';
+import { GrafanaTheme } from '@grafana/data';
+import { useStyles } from '../../themes';
+import { Icon } from '..';
+
+export interface Props {
+  label: string;
+  isOpen: boolean;
+  children: ReactNode;
+}
+
+export const CollapsableSection: FC<Props> = ({ label, isOpen, children }) => {
+  const [open, toggleOpen] = useState<boolean>(isOpen);
+  const styles = useStyles(collapsableSectionStyles);
+
+  return (
+    <div>
+      <div onClick={() => toggleOpen(!open)} className={styles.header}>
+        <Icon name={open ? 'angle-down' : 'angle-right'} size="xl" />
+        {label}
+      </div>
+      <div className={styles.content}>{open && children}</div>
+    </div>
+  );
+};
+
+const collapsableSectionStyles = (theme: GrafanaTheme) => {
+  return {
+    header: css`
+      font-size: ${theme.typography.size.lg};
+      cursor: pointer;
+    `,
+    content: css`
+      padding: ${theme.spacing.md} 0 ${theme.spacing.md} ${theme.spacing.md};
+    `,
+  };
+};

packages/grafana-ui/src/components/Forms/Checkbox.tsx
@@ -80,7 +80,7 @@ export const getCheckboxStyles = stylesFactory((theme: GrafanaTheme) => {
       background: ${theme.colors.formInputBg};
       border: 1px solid ${theme.colors.formInputBorder};
       position: absolute;
-      top: 1px;
+      top: 2px;
       left: 0;
 
       &:hover {

packages/grafana-ui/src/components/index.ts
@@ -93,6 +93,7 @@ export {
 export { Alert, AlertVariant } from './Alert/Alert';
 export { GraphSeriesToggler, GraphSeriesTogglerAPI } from './Graph/GraphSeriesToggler';
 export { Collapse, ControlledCollapse } from './Collapse/Collapse';
+export { CollapsableSection } from './Collapse/CollapsableSection';
 export { LogLabels } from './Logs/LogLabels';
 export { LogRows } from './Logs/LogRows';
 export { getLogRowStyles } from './Logs/getLogRowStyles';

pkg/services/alerting/notifier.go
@@ -23,14 +23,13 @@ var newImageUploaderProvider = func() (imguploader.ImageUploader, error) {
 
 // NotifierPlugin holds meta information about a notifier.
 type NotifierPlugin struct {
-	Type            string           `json:"type"`
-	Name            string           `json:"name"`
-	Heading         string           `json:"heading"`
-	Description     string           `json:"description"`
-	Info            string           `json:"info"`
-	OptionsTemplate string           `json:"optionsTemplate"`
-	Factory         NotifierFactory  `json:"-"`
-	Options         []NotifierOption `json:"options"`
+	Type        string           `json:"type"`
+	Name        string           `json:"name"`
+	Heading     string           `json:"heading"`
+	Description string           `json:"description"`
+	Info        string           `json:"info"`
+	Factory     NotifierFactory  `json:"-"`
+	Options     []NotifierOption `json:"options"`
 }
 
 // NotifierOption holds information about options specific for the NotifierPlugin.
@@ -45,6 +44,7 @@ type NotifierOption struct {
 	ShowWhen       ShowWhen       `json:"showWhen"`
 	Required       bool           `json:"required"`
 	ValidationRule string         `json:"validationRule"`
+	Secure         bool           `json:"secure"`
 }
 
 // InputType is the type of input that can be rendered in the frontend.
@@ -65,8 +65,8 @@ const (
 	ElementTypeInput = "input"
 	// ElementTypeSelect will render a select
 	ElementTypeSelect = "select"
-	// ElementTypeSwitch will render a switch
-	ElementTypeSwitch = "switch"
+	// ElementTypeCheckbox will render a checkbox
+	ElementTypeCheckbox = "checkbox"
 	// ElementTypeTextArea will render a textarea
 	ElementTypeTextArea = "textarea"
 )

pkg/services/alerting/notifiers/alertmanager.go
@@ -20,36 +20,6 @@ func init() {
 		Description: "Sends alert to Prometheus Alertmanager",
 		Heading:     "Alertmanager settings",
 		Factory:     NewAlertmanagerNotifier,
-		OptionsTemplate: `
-      <h3 class="page-heading">Alertmanager settings</h3>
-		<div class="gf-form max-width-30">
-            <span class="gf-form-label width-10">Url(s)</span>
-            <input type="text" required class="gf-form-input max-width-30" ng-model="ctrl.model.settings.url" placeholder="http://localhost:9093"></input>
-            <info-popover mode="right-absolute">
-              As specified in Alertmanager documentation, do not specify a load balancer here. Enter all your Alertmanager URLs comma-separated.
-            </info-popover>
-		</div>
-		<div class="gf-form max-width-30">
-        	<span class="gf-form-label width-10">Basic Auth User</span>
-            <input type="text" class="gf-form-input max-width-30" ng-model="ctrl.model.settings.basicAuthUser" placeholder=""></input>
-		</div>
-        <div class="gf-form max-width-30">
-            <span class="gf-form-label width-10">Basic Auth Password</span>
-            <div class="gf-form gf-form--grow" ng-if="!ctrl.model.secureFields.basicAuthPassword">
-                <input type="text"
-                    class="gf-form-input max-width-30"
-                    ng-init="ctrl.model.secureSettings.basicAuthPassword = ctrl.model.settings.basicAuthPassword || null; ctrl.model.settings.basicAuthPassword = null;"
-                    ng-model="ctrl.model.secureSettings.basicAuthPassword"
-                    data-placement="right">
-                </input>
-            </div>
-            <div class="gf-form" ng-if="ctrl.model.secureFields.basicAuthPassword">
-                <input type="text" class="gf-form-input max-width-18" disabled="disabled" value="configured" />
-                <a class="btn btn-secondary gf-form-btn" href="#" ng-click="ctrl.model.secureFields.basicAuthPassword = false">reset</a>
-            </div>
-        </div>
-      </div>
-    `,
 		Options: []alerting.NotifierOption{
 			{
 				Label:        "Url",
@@ -71,6 +41,7 @@ func init() {
 				Element:      alerting.ElementTypeInput,
 				InputType:    alerting.InputTypePassword,
 				PropertyName: "basicAuthPassword",
+				Secure:       true,
 			},
 		},
 	})

commit 0238a72ba643ef7e7367d98896fed672748980eb

Alerting: Fix integration key so it's stored encrypted for Pagerduty notifier (#27484)
Fixes an issue introduced by migration in #25980 which 
removed storing the Pagerduty integrating key encrypted.

pkg/services/alerting/notifiers/pagerduty.go
@@ -28,6 +28,7 @@ func init() {
 				Placeholder:  "Pagerduty Integration Key",
 				PropertyName: "integrationKey",
 				Required:     true,
+				Secure:       true,
 			},
 			{
 				Label:   "Severity",