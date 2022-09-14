87de911abe236eadcc0631eec774488e99bd1c08
jayaraj esvar <jayaraj.esvar@gmail.com>
2022-09-14 09:18:05 +0530

    updated group id to config

--- pkg/api/dtos/resourceconfiguration.go
+++ pkg/api/dtos/resourceconfiguration.go
@@ -3,6 +3,7 @@ package dtos
 type UpdateResourceConfigurationMsg struct {
 	ResourceId    int64                  `json:"resource_id"`
 	Type          string                 `json:"type"`
+	GroupId       int64                  `json:"group_id"`
 	Configuration map[string]interface{} `json:"configuration"`
 }
 
