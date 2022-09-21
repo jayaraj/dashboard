d57d73b6da88b6c425e5357cb5e5606ca3a2fa53
jayaraj esvar <jayaraj.esvar@gmail.com>
2022-09-21 23:24:17 +0530

    updated resource config

--- pkg/api/dtos/resourceconfiguration.go
+++ pkg/api/dtos/resourceconfiguration.go
@@ -1,15 +1,9 @@
 package dtos
 
 type UpdateResourceConfigurationMsg struct {
+	OrgId         int64                  `json:"org_id"`
 	ResourceId    int64                  `json:"resource_id"`
 	Type          string                 `json:"type"`
 	GroupId       int64                  `json:"group_id"`
 	Configuration map[string]interface{} `json:"configuration"`
-}
-
-type GetResourceConfigurationMsg struct {
-	ResourceId int64  `json:"resource_id"`
-	Type       string `json:"type"`
-	// swagger:ignore
-	Result map[string]interface{} `json:"result"`
 }
