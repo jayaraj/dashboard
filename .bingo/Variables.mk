71ac8687ba428384fc53b809ee8d3fc59f71bfcc
jayaraj esvar <jayaraj.esvar@gmail.com>
2025-12-20 19:34:54 +0530

    updated wire version

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -64,9 +64,9 @@ $(SWAGGER): $(BINGO_DIR)/swagger.mod
 	@echo "(re)installing $(GOBIN)/swagger-v0.30.2"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=swagger.mod -o=$(GOBIN)/swagger-v0.30.2 "github.com/go-swagger/go-swagger/cmd/swagger"
 
-WIRE := $(GOBIN)/wire-v0.5.0
+WIRE := $(GOBIN)/wire-v0.7.0
 $(WIRE): $(BINGO_DIR)/wire.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/wire-v0.5.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=wire.mod -o=$(GOBIN)/wire-v0.5.0 "github.com/google/wire/cmd/wire"
+	@echo "(re)installing $(GOBIN)/wire-v0.7.0"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=wire.mod -o=$(GOBIN)/wire-v0.7.0 "github.com/google/wire/cmd/wire"
 
